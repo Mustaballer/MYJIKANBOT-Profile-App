db4e683f687180df8d93353689f115ae5455409d
Mustafa Abdulrahman <1abdulrahmus@hdsb.ca>
2020-05-30 18:34:44 -0400

    complete code

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,94 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(myApp());
 }
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo.shade600,
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/reigen.jpg'),
+                ),
+                Text(
+                  'My Jikan Bot',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'JDA DISCORD BOT',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.indigo.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  width: 150.0,
+                  height: 20.0,
+                  child: Divider(
+                    color: Colors.indigo.shade100,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.orange,
+                    ),
+                    title: Text(
+                      '+1 123 456 789',
+                      style: TextStyle(
+                        color: Colors.indigo.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 17.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.orange,
+                    ),
+                    title: Text(
+                      'mus2003.abdul@gmail.com',
+                      style: TextStyle(
+                        color: Colors.indigo.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 17.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
